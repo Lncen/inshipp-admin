a606c329923df9f4c3d8cfec72874dd09f135b9b
逆光行走的猫 <65211886+Lncen@users.noreply.github.com>
2026-02-07 11:40:29 +0800

    chore(@vben/web-ele): 禁用 body-leading-blank 规则
    禁用 commitlint 中的 body-leading-blank 规则，注释掉该配置项以避免提交消息体前必须有空行的要求。

--- internal/lint-configs/commitlint-config/index.mjs
+++ internal/lint-configs/commitlint-config/index.mjs
@@ -90,7 +90,7 @@ const userConfig = {
      * ^^^^^^^^^^^^^^ empty line.
      * - Something here
      */
-    'body-leading-blank': [2, 'always'],
+    'body-leading-blank': [0, 'always'],
     /**
      * type[scope]: [function] description
      *
